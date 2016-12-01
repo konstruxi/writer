f324e6a8a8b30cd6c37cafefb62ef69a8ee3cf78
Yaroslaff Fedin <invizko@gmail.com>
2016-12-01 22:51:18 +0800

    Initial commit

---
+++ ckeditor/config.js
@@ -0,0 +1,51 @@
+﻿/**
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	
+	// %REMOVE_START%
+	// The configuration options below are needed when running CKEditor from source files.
+	config.plugins = 'SimpleLink,dialogui,dialog,clipboard,autolink,basicstyles,divarea,enterkey,floatingspace,entities,indent,indentlist,list,magicline,button,toolbar,undo';
+	config.skin = 'moono-dark';
+	// %REMOVE_END%
+
+	// Define changes to default configuration here.
+	// For complete reference see:
+	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
+
+	// The toolbar groups arrangement, optimized for a single toolbar row.
+	config.toolbarGroups = [
+		{ name: 'structural'},
+		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+		{ name: 'forms' },
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph',   groups: [ 'list', 'blocks', 'align', 'bidi' ] },
+		{ name: 'links' },
+		{ name: 'insert' },
+		{ name: 'styles' },
+		{ name: 'colors' },
+		{ name: 'tools' },
+		{ name: 'others' },
+		{ name: 'about' }
+	];
+
+	// The default plugins included in the basic setup define some buttons that
+	// are not needed in a basic editor. They are removed here.
+	config.removeButtons = 'Cut,Copy,Paste,Undo,Redo,Anchor,Underline,Strike,Subscript,Superscript';
+
+	// Dialog windows are also simplified.
+	config.removeDialogTabs = 'link:advanced';
+
+	config.allowedContent = 'h1 h2 blockquote a img ul li ol; section[class,disabled]'
+
+	config.shiftEnterMode = CKEDITOR.ENTER_P
+};
+
+CKEDITOR.config.height = 150;
+CKEDITOR.config.width = 'auto';
+CKEDITOR.dtd[ 'section' ][ 'section' ] = false
+CKEDITOR.dtd.$block.p = 1; 

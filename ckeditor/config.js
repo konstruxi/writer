a6779e4e929209da68d7dc912992da5fccf968d7
Yaroslaff Fedin <invizko@gmail.com>
2016-12-03 16:26:10 +0800

    better handle focus when dragging

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -47,9 +47,11 @@ CKEDITOR.config.width = 'auto';
 CKEDITOR.dtd.$block.p = 1; 
 CKEDITOR.dtd.$block.h1 = 1; 
 CKEDITOR.dtd.$block.li = 1; 
+CKEDITOR.dtd.$block.img = 1; 
 
 CKEDITOR.dtd.section = Object.create(CKEDITOR.dtd.section)
 CKEDITOR.dtd.section.section = undefined;
+
 
 
 

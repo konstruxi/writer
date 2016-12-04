06c5759c715452c9ee2074c86161dee3ed299756
Yaroslaff Fedin <invizko@gmail.com>
2016-12-04 12:57:29 +0800

    fix parent animating logic

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -48,6 +48,7 @@ CKEDITOR.dtd.$block.p = 1;
 CKEDITOR.dtd.$block.h1 = 1; 
 CKEDITOR.dtd.$block.li = 1; 
 CKEDITOR.dtd.$block.img = 1; 
+CKEDITOR.dtd.$block.blockquote = 1; 
 
 CKEDITOR.dtd.section = Object.create(CKEDITOR.dtd.section)
 CKEDITOR.dtd.section.section = undefined;

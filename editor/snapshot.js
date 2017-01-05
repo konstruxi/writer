4e869fcc5052031bee8ba1b0f509874d1f230eab
Yaroslaff Fedin <invizko@gmail.com>
2017-01-05 11:25:53 +0800

    add support for line-height normal

--- editor/snapshot.js
+++ editor/snapshot.js
@@ -192,7 +192,7 @@ Editor.Snapshot.prototype.transition = function(element, from, to, time, startTi
       else
         var spring = to[springName] = new Spring(74, 12);
     } else if (property == 'fontSize') {
-      var spring = to[springName] = new Spring(60, 12);
+      var spring = to[springName] = new Spring(100, 12);
     } else if (property == 'fontSize') {
       var spring = to[springName] = new Spring(100, 12);
     } else if (property == 'top') {
@@ -276,7 +276,7 @@ Editor.Snapshot.prototype.morph = function(snapshot, time, startTime) {
       if (to.visible) {
         if (to.currentFontSize)
           element.style.fontSize = to.currentFontSize + 'px'
-        if (to.currentLineHeight)
+        if (to.currentLineHeight && element.tagName != 'SECTION')
           element.style.lineHeight = to.currentLineHeight + 'px'
 
 
@@ -361,7 +361,7 @@ Editor.Snapshot.take = function(editor, reset, focused) {
 
     box.styles = window.getComputedStyle(elements[i]);
     box.fontSize = parseFloat(box.styles['font-size'])
-    box.lineHeight = parseFloat(box.styles['line-height'])
+    box.lineHeight = getLineHeight(box.styles['line-height'], box.fontSize)
 
     if (!box.up)
       box.up = dimensions[elements.indexOf(elements[i].parentNode)]
@@ -608,4 +608,90 @@ Editor.Snapshot.prototype.setStyle = function(element, property, value) {
   }
   box.animated = true;
   box.static = false;
+}
+
+
+
+// measure line-height value for  normal keyword 
+// for all font sizes in range of 1 ... 100
+Editor.Snapshot.lineHeights = {};
+
+var dummy = document.createElement('div')
+dummy.style.visibility = 'hidden'
+dummy.style.position = 'absolute';
+dummy.style.width = '1px';
+dummy.style.height = '1px';
+dummy.style.overflow = 'hidden';
+for (var i = 0; i < 100; i++) {
+  var stub = document.createElement('i');
+  stub.style.fontSize = i + 'px'
+  stub.style.lineHeight = 'normal'
+  stub.style.display = 'block'
+  stub.innerHTML = '&nbsp;'
+  dummy.appendChild(stub); 
+};
+document.body.appendChild(dummy);
+var dummies = dummy.getElementsByTagName('i');
+for (var i = 0; i < 100; i++) {
+  Editor.Snapshot.lineHeights[i] = dummies[i].offsetHeight
+}
+
+
+
+// code re-approriated from. added precomputed table 
+// https://github.com/twolfson/line-height/blob/master/lib/line-height.js 
+function getLineHeight(lnHeightStr, fontSize) {
+  // Grab the line-height via style
+  var lnHeight = parseFloat(lnHeightStr, 10);
+
+  /* If the lineHeight did not contain a unit (i.e. it was numeric), convert it to ems (e.g. '2.3' === '2.3em')
+  if (lnHeightStr === lnHeight + '') {
+    // Save the old lineHeight style and update the em unit to the element
+    var _lnHeightStyle = node.style.lineHeight;
+    node.style.lineHeight = lnHeightStr + 'em';
+
+    // Calculate the em based height
+    lnHeightStr = computedStyle(node, 'line-height');
+    lnHeight = parseFloat(lnHeightStr, 10);
+
+    // Revert the lineHeight style
+    if (_lnHeightStyle) {
+      node.style.lineHeight = _lnHeightStyle;
+    } else {
+      delete node.style.lineHeight;
+    }
+  }*/
+
+  // If the line-height is "normal", calculate by font-size
+  if (lnHeightStr === 'normal') {
+    return Editor.Snapshot.lineHeights[Math.floor(fontSize)]
+  }
+  // If the lineHeight is in `pt`, convert it to pixels (4px for 3pt)
+  // DEV: `em` units are converted to `pt` in IE6
+  // Conversion ratio from https://developer.mozilla.org/en-US/docs/Web/CSS/length
+  if (lnHeightStr.indexOf('pt') !== -1) {
+    lnHeight *= 4;
+    lnHeight /= 3;
+  } else if (lnHeightStr.indexOf('mm') !== -1) {
+  // Otherwise, if the lineHeight is in `mm`, convert it to pixels (96px for 25.4mm)
+    lnHeight *= 96;
+    lnHeight /= 25.4;
+  } else if (lnHeightStr.indexOf('cm') !== -1) {
+  // Otherwise, if the lineHeight is in `cm`, convert it to pixels (96px for 2.54cm)
+    lnHeight *= 96;
+    lnHeight /= 2.54;
+  } else if (lnHeightStr.indexOf('in') !== -1) {
+  // Otherwise, if the lineHeight is in `in`, convert it to pixels (96px for 1in)
+    lnHeight *= 96;
+  } else if (lnHeightStr.indexOf('pc') !== -1) {
+  // Otherwise, if the lineHeight is in `pc`, convert it to pixels (12pt for 1pc)
+    lnHeight *= 16;
+  }
+
+  // Continue our computation
+  lnHeight = Math.round(lnHeight);
+
+
+  // Return the calculated height
+  return lnHeight;
 }

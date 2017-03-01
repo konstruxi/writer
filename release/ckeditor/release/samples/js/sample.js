3c8a5eefabaceb4a0b1eb30b63761477ea1dc3a4
Yaroslaff Fedin <invizko@gmail.com>
2017-03-01 14:59:27 +0800

    compiled ckeditor

---
+++ release/ckeditor/release/samples/js/sample.js
@@ -0,0 +1,2 @@
+﻿CKEDITOR.env.ie&&9>CKEDITOR.env.version&&CKEDITOR.tools.enableHtml5Elements(document);CKEDITOR.config.height=150;CKEDITOR.config.width="auto";
+var initSample=function(){var a;a="%REV%"==CKEDITOR.revision?!0:!!CKEDITOR.plugins.get("wysiwygarea");var c=!!CKEDITOR.plugins.get("bbcode");return function(){var b=CKEDITOR.document.getById("editor");c&&b.setHtml("Hello world!\n\nI'm an instance of [url\x3dhttp://ckeditor.com]CKEditor[/url].");a?CKEDITOR.replace("editor"):(b.setAttribute("contenteditable","true"),CKEDITOR.inline("editor"))}}();
---
+++ release/ckeditor/release/skins/moono-dark/skin.js
@@ -0,0 +1,1 @@
+﻿CKEDITOR.skin.name="moono-dark";CKEDITOR.skin.ua_editor="ie,iequirks,ie7,ie8,gecko";CKEDITOR.skin.ua_dialog="ie,iequirks,ie7,ie8";
